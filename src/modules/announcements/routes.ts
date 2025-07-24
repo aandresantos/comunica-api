98fbfe6bdb5cce1c6ba39398d47b1a0e061c3789
André Santos <andrelucianodossantosjunior@gmail.com>
2025-07-24 17:33:00 -0300

    chore: created base for announcements routes

--- src/modules/announcements/routes.ts
+++ src/modules/announcements/routes.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import { AnnouncementsService, IAnnouncementsService } from "./service";
 import {
   AnnouncementsRepository,
@@ -9,7 +9,37 @@ const repository: IAnnouncementsRepository = new AnnouncementsRepository();
 const service: IAnnouncementsService = new AnnouncementsService(repository);
 
 export async function announcementsRoutes(app: FastifyInstance) {
+  // TODO: implementar camada de handler
   app.get("/", async () => {
     return service.listAnnouncements();
+  });
+
+  app.get("/:id", async (req, reply) => {
+    const { id } = req.params as FastifyRequest<{ Params: { id: string } }>;
+
+    return { data: id };
+  });
+
+  app.post("/", async (req, reply) => {
+    const { body } = req.body as FastifyRequest<{
+      Body: { id: string };
+    }>;
+
+    return { data: "criado com sucesso" };
+  });
+
+  app.post("/:id", async (req, reply) => {
+    const { id } = req.params as FastifyRequest<{ Params: { id: string } }>;
+    const { body } = req.body as FastifyRequest<{
+      Body: { id: string };
+    }>;
+
+    return { data: "atualizado com sucesso" };
+  });
+
+  app.delete("/:id", async (req, reply) => {
+    const { id } = req.params as FastifyRequest<{ Params: { id: string } }>;
+
+    return { data: "deletado com sucesso" };
   });
 }
